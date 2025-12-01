6c1de13d0bb32a956f93e8e0138ba07add3ea9a9
FlorinAndrei <901867+FlorinAndrei@users.noreply.github.com>
2025-11-30 17:56:55 -0800

    create headless run script

---
+++ run.sh
@@ -0,0 +1,18 @@
+if [ $# -lt 1 ]; then
+	echo "Usage:"
+	echo
+	echo "Run a HuggingFace model (replace model name with your choice):"
+	echo "$0 --model google/gemma-3-4b-it"
+	echo
+	echo "Run a local model with a HuggingFace template (replace model name and path with your choices):"
+	echo "$0 --model google/gemma-3-4b-it --local_model_path local_gemma4"
+	echo
+	exit 1
+fi
+
+exit
+
+rm -rf output stdout.log stderr.log
+source .venv/bin/activate
+# https://github.com/triton-lang/triton/issues/8539
+TRITON_PTXAS_PATH=`which ptxas` python -m lcb_runner.runner.main --gpu_memory_utilization 0.65 --scenario codegeneration --evaluate --release_version release_v1 $@ 1>stdout.log 2>stderr.log &
